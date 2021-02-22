48d457456f641a1854230c1158cea5755b90e956
Brad Nussbaum <brad@graphfoundation.org>
2021-02-22 18:32:54 -0500

    BROWSER-17 - Update About page for ONgDB

--- src/browser/modules/Sidebar/About.jsx
+++ src/browser/modules/Sidebar/About.jsx
@@ -34,26 +34,31 @@ import { getVersion, getEdition } from 'shared/modules/dbMeta/dbMetaDuck'
 
 const About = ({ serverVersion, serverEdition }) => (
   <Drawer id="db-about">
-    <DrawerHeader>About Neo4j</DrawerHeader>
+    <DrawerHeader>About ONgDB</DrawerHeader>
     <DrawerBody>
       <DrawerSection>
         <DrawerSubHeader>
           Made by{' '}
-          <a target="_blank" href="http://neo4j.com/">
-            Neo4j, Inc
+          <a target="_blank" href="https://graphfoundation.org/">
+            Graph Foundation, Inc.
           </a>
         </DrawerSubHeader>
       </DrawerSection>
       <DrawerSection>
-        <DrawerSectionBody>Copyright &#169; 2002-2020</DrawerSectionBody>
+        <DrawerSectionBody>
+          Copyright &#169; 2018-2020 Graph Foundation, Inc.
+        </DrawerSectionBody>
+        <DrawerSectionBody>
+          Copyright &#169; 2002-2020 Neo4j Sweden AB
+        </DrawerSectionBody>
       </DrawerSection>
       <DrawerSection>
         <DrawerSubHeader>You are running</DrawerSubHeader>
         <DrawerSectionBody>
           <p>
-            Neo4j Browser version:{' '}
+            ONgDB Browser version:{' '}
             <a
-              href={`https://github.com/neo4j/neo4j-browser/releases/tag/${version}`}
+              href={`https://github.com/graphfoundation/ongdb-browser/releases/tag/${version}`}
               target="_blank"
             >
               {version}
@@ -61,7 +66,7 @@ const About = ({ serverVersion, serverEdition }) => (
           </p>
           <Render if={serverVersion && serverEdition}>
             <p>
-              Neo4j Server version:{' '}
+              ONgDB Server version:{' '}
               <a target="_blank" href={asChangeLogUrl(serverVersion)}>
                 {serverVersion}
               </a>{' '}
@@ -70,10 +75,10 @@ const About = ({ serverVersion, serverEdition }) => (
           </Render>
           <p>
             <a
-              href="https://github.com/neo4j/neo4j-browser/wiki/changelog"
+              href="https://github.com/graphfoundation/ongdb-browser/wiki/changelog"
               target="_blank"
             >
-              Neo4j Browser Changelog
+              ONgDB Browser Changelog
             </a>
           </p>
         </DrawerSectionBody>
@@ -88,58 +93,42 @@ const About = ({ serverVersion, serverEdition }) => (
           <a target="_blank" href="http://www.gnu.org/licenses/agpl-3.0.html">
             AGPL
           </a>{' '}
-          for Open Source, and{' '}
-          <a target="_blank" href="https://neo4j.com/licensing/">
-            NTCL
-          </a>{' '}
-          Commercial.
         </DrawerSectionBody>
       </DrawerSection>
       <DrawerSection>
         <DrawerSubHeader>Participate</DrawerSubHeader>
         <DrawerSectionBody>
-          Discuss on{' '}
-          <a target="_blank" href="https://community.neo4j.com/">
-            Neo4j Community Forum
-          </a>{' '}
-          <br />
           Ask questions at{' '}
           <a
             target="_blank"
-            href="http://stackoverflow.com/questions/tagged/neo4j"
+            href="http://stackoverflow.com/questions/tagged/ongdb"
           >
             Stack Overflow
           </a>
           <br />
-          Visit a local{' '}
-          <a target="_blank" href="http://neo4j.meetup.com/">
-            Meetup Group
-          </a>
-          <br />
           Contribute code to{' '}
-          <a target="_blank" href="http://github.com/neo4j">
-            Neo4j
+          <a target="_blank" href="http://github.com/graphfoundation">
+            Graph Foundation
           </a>{' '}
           or{' '}
-          <a target="_blank" href="http://github.com/neo4j/neo4j-browser">
-            Neo4j Browser
+          <a
+            target="_blank"
+            href="http://github.com/graphfoundation/ongdb-browser"
+          >
+            ONgDB Browser
           </a>
           <br />
-          Send us your Browser feedback via{' '}
-          <a href="mailto:browser@neotechnology.com?subject=Neo4j Browser feedback">
-            email
-          </a>
         </DrawerSectionBody>
       </DrawerSection>
       <DrawerSection>
         <DrawerSubHeader>Thanks</DrawerSubHeader>
         <DrawerSectionBody>
-          Neo4j wouldn't be possible without a fantastic community. Thanks for
+          ONgDB wouldn't be possible without a fantastic community. Thanks for
           all the feedback, discussions and contributions.
         </DrawerSectionBody>
       </DrawerSection>
     </DrawerBody>
-    <DrawerFooter>With &#9829; from Sweden.</DrawerFooter>
+    <DrawerFooter>Made with &#9829; worldwide.</DrawerFooter>
   </Drawer>
 )
 
@@ -151,7 +140,7 @@ const asChangeLogUrl = serverVersion => {
   const releaseTag = tokenisedServerVersion.join('')
   const urlServerVersion =
     serverVersion && tokenisedServerVersion.splice(0, 2).join('.')
-  return `https://github.com/neo4j/neo4j/wiki/Neo4j-${urlServerVersion}-changelog#${releaseTag}`
+  return `https://github.com/graphfoundation/ongdb/wiki/ONgDB-${urlServerVersion}-changelog#${releaseTag}`
 }
 
 const mapStateToProps = state => {
